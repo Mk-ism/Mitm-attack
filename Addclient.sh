66d012d841ffb6e7a2e64930fd8123b44df1fdd8
Aman <50996845+Average-stu@users.noreply.github.com>
2020-07-09 16:12:28 +0530

    For Mitigation part

---
+++ Addclient.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#!/bin/sh
+
+clear
+printf "\e  ADDING THE CLIENT \e[0m\n"
+pivpn -a
+printf "\e THE PATH WILL BE - /home/pi/ovpns "
+
---
+++ yourblacklist.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#!/bin/sh
+
+banner() {
+clear
+printf "\e[1;92m\e[0m\e[1;77m\e[0m\e[1;92m\e[0m\e[1;93m USE THIS SCRIPT TO ADD YOUR MANGEMENT OF YOUR DNS\e[0m\n "
+printf ""
+
+printf "\n"
+printf "\e[1;92m\e[0m\e[1;77m\e[0m\e[1;92m\e[0m\e[1;93m WHAT YOU WANNA DO\e[0m\n"
+printf ""
+}
+menu() {
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m BLOCK\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m UNBLOCK\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m WHITELIST\e[0m\n"
+printf  "\n"
+printf "\e[1;93m[\e[0m\e[1;77m99\e[0m\e[1;93m]\e[0m\e[1;77m DONE YOUR SETTING\e[0m\n"
+printf "\n"
+read -p $'\e[1;92m[*] Choose an option:\e[0m\e[1;77m ' option
+
+if [[ $option == 1 || $option == 01 ]]; then
+printf "\n"
+printf "\n"
+printf "\e[1;92m\e[0m\e[1;77m\e[0m\e[1;92m\e[0m\e[1;93m BLOCK\e[0m\n"
+read -p 'DOMAIN: ' domain
+pihole --regex '(^|\.)'$domain'\.com$'
+sleep 2
+clear
+menu
+
+
+elif [[ $option == 2 || $option == 02 ]]; then
+printf "\n"
+printf "\n"
+printf "\e[1;92m\e[0m\e[1;77m\e[0m\e[1;92m\e[0m\e[1;93m UNBLOCK\e[0m\n"
+read -p 'DOMAIN:' domain
+pihole -b -d --regex '(^|\.)'$domain'\.com$'
+sleep 2
+clear
+menu
+
+elif [[ $option  == 99 ]]; then
+exit 1
+else
+printf "\e[5;93m[\e[1;77m!\e[0m\e[1;93m] Invalid option!\e[0m"
+sleep 0.5
+clear
+menu
+banner
+fi
+}
+banner
+menu
